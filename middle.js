cb9cca63eb4cc3c1e39ce2bd57f258513bf0c4ad
yakupenesguven <yakupenes.guven@gmail.com>
2023-06-10 21:43:37 +0300

    complex queries added

--- middle.js
+++ middle.js
@@ -1,83 +1,149 @@
 async function getMoviesByDirector(director) {
-  const response = await fetch(`http://localhost:3000/moviesByDirector?director=${director}`);
-  const jsonData = await response.json();
-  return jsonData.results;
+    const response = await fetch(`http://localhost:3000/moviesByDirector?director=${director}`);
+    const jsonData = await response.json();
+    return jsonData.results;
 }
 
 function displayMoviesByDirector() {
-  const director = document.getElementById("director").value;
+    const director = document.getElementById("director").value;
 
-  getMoviesByDirector(director).then((movies) => {
-    const ul = document.getElementById("movies-by-director");
-    ul.innerHTML = ""; // Clear the previous results
+    getMoviesByDirector(director).then((movies) => {
+        const ul = document.getElementById("movies-by-director");
+        ul.innerHTML = ""; // Clear the previous results
 
-    movies.forEach((movie) => {
-      const li = document.createElement("li");
-      li.textContent = movie.title;
-      ul.appendChild(li);
+        movies.forEach((movie) => {
+            const li = document.createElement("li");
+            li.textContent = movie.title;
+            ul.appendChild(li);
+        });
     });
-  });
 }
 
 async function getMoviesByActor(actor) {
-  const response = await fetch(`http://localhost:3000/moviesByActor?actor=${actor}`);
-  const jsonData = await response.json();
-  return jsonData.results;
+    const response = await fetch(`http://localhost:3000/moviesByActor?actor=${actor}`);
+    const jsonData = await response.json();
+    return jsonData.results;
 }
 
 function displayMoviesByActor() {
-  const actor = document.getElementById("actor").value;
+    const actor = document.getElementById("actor").value;
 
-  getMoviesByActor(actor).then((movies) => {
-    const ul = document.getElementById("movies-by-actor");
-    ul.innerHTML = ""; // Clear the previous results
+    getMoviesByActor(actor).then((movies) => {
+        const ul = document.getElementById("movies-by-actor");
+        ul.innerHTML = ""; // Clear the previous results
 
-    movies.forEach((movie) => {
-      const li = document.createElement("li");
-      li.textContent = movie.title;
-      ul.appendChild(li);
+        movies.forEach((movie) => {
+            const li = document.createElement("li");
+            li.textContent = movie.title;
+            ul.appendChild(li);
+        });
     });
-  });
 }
 
 async function getMoviesReleasedAfter(date) {
-  const response = await fetch(`http://localhost:3000/moviesReleasedAfter?date=${date}`);
-  const jsonData = await response.json();
-  return jsonData.results;
+    const response = await fetch(`http://localhost:3000/moviesReleasedAfter?date=${date}`);
+    const jsonData = await response.json();
+    return jsonData.results;
 }
 
 function displayMoviesReleasedAfter() {
-  const date = document.getElementById("release-date").value;
+    const date = document.getElementById("release-date").value;
 
-  getMoviesReleasedAfter(date).then((movies) => {
-    const ul = document.getElementById("movies-released-after");
-    ul.innerHTML = ""; // Clear the previous results
+    getMoviesReleasedAfter(date).then((movies) => {
+        const ul = document.getElementById("movies-released-after");
+        ul.innerHTML = ""; // Clear the previous results
 
-    movies.forEach((movie) => {
-      const li = document.createElement("li");
-      li.textContent = movie.title;
-      ul.appendChild(li);
+        movies.forEach((movie) => {
+            const li = document.createElement("li");
+            li.textContent = movie.title;
+            ul.appendChild(li);
+        });
     });
-  });
 }
 
 async function getMoviesByGenre(genre) {
-  const response = await fetch(`http://localhost:3000/moviesByGenre?genre=${genre}`);
+    const response = await fetch(`http://localhost:3000/moviesByGenre?genre=${genre}`);
+    const jsonData = await response.json();
+    return jsonData.results;
+}
+
+function displayMoviesByGenre() {
+    const genre = document.getElementById("genre").value;
+
+    getMoviesByGenre(genre).then((movies) => {
+        const ul = document.getElementById("movies-by-genre");
+        ul.innerHTML = ""; // Clear the previous results
+
+        movies.forEach((movie) => {
+            const li = document.createElement("li");
+            li.textContent = movie.title;
+            ul.appendChild(li);
+        });
+    });
+}
+
+
+async function getDirectorsWithMultipleGenres() {
+  const response = await fetch("http://localhost:3000/directorsWithMultipleGenres");
   const jsonData = await response.json();
   return jsonData.results;
 }
 
-function displayMoviesByGenre() {
-  const genre = document.getElementById("genre").value;
-
-  getMoviesByGenre(genre).then((movies) => {
-    const ul = document.getElementById("movies-by-genre");
+async function getActorsWithDirectorsInSameYear() {
+  const response = await fetch("http://localhost:3000/actorsWithDirectorsInSameYear");
+  const jsonData = await response.json();
+  return jsonData.results;
+}
+function displayActorsWithDirectorsInSameYear() {
+  getActorsWithDirectorsInSameYear().then((actors) => {
+    const ul = document.getElementById("actors-with-directors-in-same-year");
     ul.innerHTML = ""; // Clear the previous results
 
-    movies.forEach((movie) => {
+    actors.forEach((actor) => {
       const li = document.createElement("li");
-      li.textContent = movie.title;
+      li.textContent = actor.name;
       ul.appendChild(li);
     });
   });
 }
+
+
+async function getMoviesWithHigherRating() {
+  const response = await fetch("http://localhost:3000/moviesWithHigherRating");
+  const jsonData = await response.json();
+  return jsonData.results;
+}
+function displayMoviesWithHigherRating() {
+  getMoviesWithHigherRating().then((movies) => {
+    const ul = document.getElementById("movies-with-higher-rating");
+    ul.innerHTML = ""; // Clear the previous results
+
+    movies.forEach((movie) => {
+      const li = document.createElement("li");
+      li.textContent = movie.name;
+      ul.appendChild(li);
+    });
+  });
+}
+
+
+async function getGenresWithHighestAverageRating() {
+  const response = await fetch("http://localhost:3000/genresWithHighestAverageRating");
+  const jsonData = await response.json();
+  return jsonData.results;
+}
+function displayGenresWithHighestAverageRating() {
+  getGenresWithHighestAverageRating().then((genres) => {
+    const ul = document.getElementById("genres-with-highest-average-rating");
+    ul.innerHTML = ""; // Clear the previous results
+
+    genres.forEach((genre) => {
+      const li = document.createElement("li");
+      li.textContent = genre.genre;
+      ul.appendChild(li);
+    });
+  });
+}
+
+
+
--- server.js
+++ server.js
@@ -7,105 +7,205 @@ const path = require("path");
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "mini_movie_imdb",
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "mini_movie_imdb",
 });
 
 pool.query("SELECT * FROM movie", (error, data) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-  console.log("Connection made to the database");
+    if (error) {
+        console.error(error);
+        return;
+    }
+    console.log("Connection made to the database");
 });
 
 function getMoviesByDirector(director, callback) {
-  pool.query(
-    "SELECT * FROM MOVIE m INNER JOIN DIRECTOR d ON m.director_id = d.director_id WHERE d.name LIKE ?",
-    [`%${director}%`],
-    (error, results) => {
-      if (error) throw error;
-      callback(results);
-    }
-  );
+    pool.query(
+        "SELECT * FROM MOVIE m INNER JOIN DIRECTOR d ON m.director_id = d.director_id WHERE d.name LIKE ?",
+        [`%${director}%`],
+        (error, results) => {
+            if (error) throw error;
+            callback(results);
+        }
+    );
 }
-
 
 
 function getMoviesByActor(actorName, callback) {
-  pool.query(
-    "SELECT m.* FROM MOVIE m INNER JOIN PLAYS_IN_MOVIES pim ON m.film_id = pim.film_id INNER JOIN ACTOR a ON pim.actor_id = a.actor_id WHERE a.name LIKE ?",
-    [`%${actorName}%`],
-    (error, results) => {
-      if (error) throw error;
-      callback(results);
-    }
-  );
+    pool.query(
+        "SELECT m.* FROM MOVIE m INNER JOIN PLAYS_IN_MOVIES pim ON m.film_id = pim.film_id INNER JOIN ACTOR a ON pim.actor_id = a.actor_id WHERE a.name LIKE ?",
+        [`%${actorName}%`],
+        (error, results) => {
+            if (error) throw error;
+            callback(results);
+        }
+    );
 }
-
-
 
 
 function getMoviesReleasedAfter(date, callback) {
-  pool.query(
-    "SELECT * FROM MOVIE WHERE release_date > ?",
-    [date],
-    (error, results) => {
-      if (error) throw error;
-      callback(results);
-    }
-  );
+    pool.query(
+        "SELECT * FROM MOVIE WHERE release_date > ?",
+        [date],
+        (error, results) => {
+            if (error) throw error;
+            callback(results);
+        }
+    );
 }
 
 function getMoviesByGenre(genre, callback) {
-  pool.query(
-    "SELECT * FROM MOVIE m INNER JOIN GENRE_MOVIES gm ON m.film_id = gm.film_id WHERE gm.genre = ?",
-    [genre],
-    (error, results) => {
-      if (error) throw error;
-      callback(results);
-    }
-  );
+    pool.query(
+        "SELECT * FROM MOVIE m INNER JOIN GENRE_MOVIES gm ON m.film_id = gm.film_id WHERE gm.genre = ?",
+        [genre],
+        (error, results) => {
+            if (error) throw error;
+            callback(results);
+        }
+    );
+}
+// complex queries
+
+function getDirectorsWithMultipleGenres(callback) {
+  const query = `
+    SELECT D.name
+    FROM DIRECTOR D
+    JOIN MOVIE M ON D.director_id = M.director_id
+    JOIN GENRE_MOVIES GM ON M.film_id = GM.film_id
+    GROUP BY D.director_id
+    HAVING COUNT(DISTINCT GM.genre) >= 3;
+  `;
+
+  pool.query(query, (error, results) => {
+    if (error) throw error;
+    callback(results);
+  });
+}
+function getActorsWithDirectorsInSameYear(callback) {
+  const query = `
+    SELECT A.name
+    FROM ACTOR A
+    JOIN PLAYS_IN_MOVIES PIM ON A.actor_id = PIM.actor_id
+    JOIN MOVIE M ON PIM.film_id = M.film_id
+    JOIN DIRECTOR D1 ON M.director_id = D1.director_id
+    JOIN DIRECTOR D2 ON M.director_id <> D2.director_id
+    WHERE YEAR(D1.birth_date) = YEAR(D2.birth_date)
+    GROUP BY A.actor_id
+    HAVING COUNT(DISTINCT M.director_id) >= 2;
+  `;
+
+  pool.query(query, (error, results) => {
+    if (error) throw error;
+    callback(results);
+  });
+}
+function getMoviesWithHigherRating(callback) {
+  const query = `
+    SELECT name
+    FROM MOVIE
+    WHERE rating > (SELECT AVG(rating) FROM MOVIE);
+  `;
+
+  pool.query(query, (error, results) => {
+    if (error) throw error;
+    callback(results);
+  });
 }
 
+function getGenresWithHighestAverageRating(callback) {
+  const query = `
+    SELECT GM.genre
+    FROM GENRE_MOVIES GM
+    JOIN MOVIE M ON GM.film_id = M.film_id
+    WHERE M.release_date >= DATE_SUB(NOW(), INTERVAL 1 YEAR)
+    GROUP BY GM.genre
+    HAVING AVG(M.rating) = (
+      SELECT MAX(avg_rating)
+      FROM (
+        SELECT AVG(M.rating) AS avg_rating
+        FROM GENRE_MOVIES GM
+        JOIN MOVIE M ON GM.film_id = M.film_id
+        WHERE M.release_date >= DATE_SUB(NOW(), INTERVAL 1 YEAR)
+        GROUP BY GM.genre
+      ) AS avg_ratings
+    );
+  `;
+
+  pool.query(query, (error, results) => {
+    if (error) throw error;
+    callback(results);
+  });
+}
+
+
+
+
+
+
+
+// linking
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/moviesByDirector", (req, res) => {
-  const director = req.query.director;
-  getMoviesByDirector(director, (results) => {
-    res.send({ movies: results });
-  });
+    const director = req.query.director;
+    getMoviesByDirector(director, (results) => {
+        res.send({movies: results});
+    });
 });
 
 app.get("/moviesByActor", (req, res) => {
-  const actor = req.query.actor;
-  getMoviesByActor(actor, (results) => {
-    res.send({ movies: results });
-  });
+    const actor = req.query.actor;
+    getMoviesByActor(actor, (results) => {
+        res.send({movies: results});
+    });
 });
 
 app.get("/moviesReleasedAfter", (req, res) => {
-  const date = req.query.date;
-  getMoviesReleasedAfter(date, (results) => {
+    const date = req.query.date;
+    getMoviesReleasedAfter(date, (results) => {
+        res.send({movies: results});
+    });
+});
+
+app.get("/moviesByGenre", (req, res) => {
+    const genre = req.query.genre;
+    getMoviesByGenre(genre, (results) => {
+        res.send({movies: results});
+    });
+});
+
+app.get("/directorsWithMultipleGenres", (req, res) => {
+  getDirectorsWithMultipleGenres((results) => {
+    res.send({ directors: results });
+  });
+});
+
+app.get("/actorsWithDirectorsInSameYear", (req, res) => {
+  getActorsWithDirectorsInSameYear((results) => {
+    res.send({ actors: results });
+  });
+});
+
+app.get("/moviesWithHigherRating", (req, res) => {
+  getMoviesWithHigherRating((results) => {
     res.send({ movies: results });
   });
 });
 
-app.get("/moviesByGenre", (req, res) => {
-  const genre = req.query.genre;
-  getMoviesByGenre(genre, (results) => {
-    res.send({ movies: results });
+app.get("/genresWithHighestAverageRating", (req, res) => {
+  getGenresWithHighestAverageRating((results) => {
+    res.send({ genres: results });
   });
 });
 
 app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+    console.log("Server is running on http://localhost:3000");
 });
